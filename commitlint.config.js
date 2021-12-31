32112f4bee14c5feaaa3de95da551d0960f7779e
Julian Vallee <julian@vallee-design.de>
2021-12-31 13:09:55 +0100

    build(git): improve build and release process
    Add commit linting, version bumping and release tagging

---
+++ commitlint.config.js
@@ -0,0 +1,20 @@
+/**
+ * Possible types for config-conventional:
+ * [
+ *   'build',
+ *   'chore',
+ *   'ci',
+ *   'docs',
+ *   'feat',
+ *   'fix',
+ *   'perf',
+ *   'refactor',
+ *   'revert',
+ *   'style',
+ *   'test'
+ * ]
+ *
+ * @see https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional
+ */
+
+module.exports = {extends: ['@commitlint/config-conventional']};
